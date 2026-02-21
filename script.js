66a5de24c527a5d9f571413181b65c77e0b80824
Eren <erenerkaya2006@gmail.com>
2026-02-21 20:29:49 +0300

    navbarfinish
    navbar bitti css hariç

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Butonu ve menüyü HTML içinden seçiyoruz
+  const profileBtn = document.getElementById('profileBtn');
+  const profileMenu = document.getElementById('profileMenu');
+
+  // Profil resmine tıklandığında çalışacak kod
+  profileBtn.addEventListener('click', function(event) {
+    event.stopPropagation(); // Tıklamanın dışarı taşmasını engeller
+    profileMenu.classList.toggle('show'); // 'show' sınıfını ekle/çıkar (Aç/Kapat)
+  });
+
+  // Kullanıcı sayfada menü dışında herhangi bir yere tıklarsa menüyü kapat
+  window.addEventListener('click', function() {
+    if (profileMenu.classList.contains('show')) {
+      profileMenu.classList.remove('show');
+    }
+  });
 
